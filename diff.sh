12ece90d5aa876179682be7575053234f3621b16
jitum <80806405+jitum@users.noreply.github.com>
2021-08-19 18:03:17 +0530

    Update diff.sh

--- diff.sh
+++ diff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #input of Array
-arr=(10 8 20 100 1 12 55)
+arr=(10 30 16 90 1 9)
 
 #For max diff we will sort arry and find diff bw first and last element
 echo "Array "
@@ -9,10 +9,12 @@ echo ${arr[*]}
 ##arry length
 alen=${#arr[@]}
 
+sum=0
+
 # Performing Bubble sort 
 for ((i = 0; i<$alen; i++))
 do
-      
+    
     for((j = 0; j<$alen-i-1; j++))
     do
       
@@ -24,6 +26,8 @@ do
             arr[$((j+1))]=$temp
         fi
     done
+    
+    
 done
   
 #echo "Array in sorted order :"
@@ -31,3 +35,20 @@ done
 
 maxdiff=`expr ${arr[$alen-1]} - ${arr[0]} `
 echo "Max diff is : "$maxdiff
+
+
+
+
+for ((i = 0; i<$alen; i++))
+do
+    sum=`expr $sum + ${arr[$i]} `
+       
+done
+
+
+
+echo "sum is $sum"
+echo "number of elements $alen"
+avg=0.0
+avg=`expr $sum / $alen `
+echo "Avrage is :$avg"
